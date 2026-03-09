4caf1e26deef08b74f06df97ec84994ec7341113
Aleksandr Vindman <vindmanaleksandr@gmail.com>
2026-03-09 12:19:29 +0300

    Added docker-compose

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ import { fileURLToPath } from 'url';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+const HOST = process.env.HOST || '0.0.0.0';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -14,6 +15,6 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../src/index.html'));
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, HOST, () => {
   console.log(`Server running at http://localhost:${PORT}`);
 });
