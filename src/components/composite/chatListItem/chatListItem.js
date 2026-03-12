4b53c426253ec1b26d563cd016da75d4faaf8aff
Pavel <Ladygin_2005q@mail.ru>
2026-03-12 20:13:14 +0300

    fix rebase

--- src/components/composite/chatListItem/chatListItem.js
+++ src/components/composite/chatListItem/chatListItem.js
@@ -10,15 +10,11 @@ export class ChatListItem extends BaseForm {
         super(props);
         this.tempName = "components/composite/chatListItem/chatListItem";
     };
-<<<<<<< HEAD
-    
-=======
 
     /**
      * Выделяет указанный чат, сбрасывая выделение остальных.
      * @param {ChatItem} selectedItem - Выбранный элемент.
      */
->>>>>>> c4e77b3 (add docs)
     selectChat(selectedItem) {
         this.chatItems.forEach(item => {
             item.element.className = "chat-item--default";
--- src/components/composite/chatListWrapper/chatListWrapper.js
+++ src/components/composite/chatListWrapper/chatListWrapper.js
@@ -5,23 +5,11 @@ import { ChatListItem } from "../chatListItem/chatListItem.js";
  * Обёртка для списка чатов (ChatListItem).
  */
 export class ChatListWrapper extends BaseForm {
-<<<<<<< HEAD
     constructor(props={}) {
         super(props);
         this.tempName = "components/composite/chatListWrapper/chatListWrapper";
     }
 
-=======
-    render() {
-        const chatWrapper = document.createElement('div');
-        chatWrapper.className = "chat-list-wrapper";
-        return chatWrapper;
-    };
-    
-    /**
-     * Монтирует дочерние компоненты и находит элемент ошибки формы.
-     */
->>>>>>> c4e77b3 (add docs)
     afterMount() {
         this.chatList = new ChatListItem();
         this.chatList.mount(this.element);
