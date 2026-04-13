0a7c0b0f63c0f30b31f68dada812185e5ca0f891
Pavel <Ladygin_2005q@mail.ru>
2026-04-13 04:00:17 +0300

    fix: hovers

--- src/components/composite/profileMainInfoBlock/profileMainInfoBlock.ts
+++ src/components/composite/profileMainInfoBlock/profileMainInfoBlock.ts
@@ -1,10 +1,16 @@
 import { BaseComponent, IBaseComponentProps } from "../../../core/base/baseComponent";
 import { ProfileMainInfo } from "../../../types/profile";
 import { Avatar } from "../../ui/avatar/avatar";
-import { Button } from "../../ui/button/button";
 import { SettingsFullNameForm } from "../settingsFullNameForm/settingsFullNameForm";
 import template from './profileMainInfoBlock.hbs'
 
+/**
+ * @interface ProfileMainInfoBlockProps - Свойства основного блока профиля.
+ * @property {ProfileMainInfo} profileMainInfo - Информация о профиле.
+ * @property {"contact" | "private_profile"} type - Тип отображения (чужой профиль или свой).
+ * @property {Function} [onInput] - Колбэк при изменении имени/фамилии.
+ * @property {Function} [onAvatarEditClick] - Колбэк при клике на редактирование аватарки.
+ */
 interface ProfileMainInfoBlockProps extends IBaseComponentProps {
     profileMainInfo: ProfileMainInfo;
     type: "contact" | "private_profile";
@@ -12,68 +18,83 @@ interface ProfileMainInfoBlockProps extends IBaseComponentProps {
     onAvatarEditClick?: (avatarWrapElement: HTMLElement) => void;
 };
 
+/**
+ * Компонент основного блока информации профиля.
+ * Отображает аватарку и форму (или текст) с именем пользователя.
+ */
 export class ProfileMainInfoBlock extends BaseComponent<ProfileMainInfoBlockProps> {
     private profileAvatar: Avatar | null = null;
     private settingsFullNameForm: SettingsFullNameForm | null = null;
-    private wrapOverlay: Button | null = null;
 
     constructor(props: ProfileMainInfoBlockProps) {
         super(props);
     };
 
-    getTemplate() {
-        return template;
+    /**
+     * @override
+     */
+    public getTemplate(): (context?: any) => string {
+        return (context: any) => template({
+            ...this.props.profileMainInfo,
+            isPrivate: this.props.type === "private_profile",
+            ...context
+        });
     }
 
+    /**
+     * @override
+     */
     protected afterMount(): void {
         if (!this.element) return;
 
-        switch (this.props.type) {
-            case "contact":
-                this.element.className = "profile-main-info-block";
-                this.profileAvatar = new Avatar({
-                    src: this.props.profileMainInfo.avatarUrl,
-                    class: "profile-avatar"
-                });
-                this.profileAvatar.mount(this.element!);
-                const userFullName = document.createElement('p');
-                userFullName.className = 'contact-user-full-name';
-                userFullName.textContent = `${this.props.profileMainInfo.firstName} ${this.props.profileMainInfo.lastName}`;
-                this.element!.appendChild(userFullName);
-                break;
-            case "private_profile":
-                this.element.className = "settings-profile-main-info-block";
-                const avatarWrapper = document.createElement('div');
-                avatarWrapper.className = "settings-avatar-wrap";
-                this.element!.appendChild(avatarWrapper);
-                this.profileAvatar = new Avatar({
-                    src: this.props.profileMainInfo.avatarUrl,
-                    class: "private-profile-avatar"
-                });
-                this.profileAvatar.mount(avatarWrapper);
-                this.wrapOverlay = new Button({
-                    icon: "/assets/images/icons/photoEdit.svg",
-                    class: "settings-avatar-wrap__overlay",
-                    type: "button",
-                    onClick: () => {
-                        this.props.onAvatarEditClick?.(avatarWrapper);
-                    },
-                });
-                this.wrapOverlay.mount(avatarWrapper);
+        const avatarSlot = this.element.querySelector('[data-component="profile-avatar-slot"]');
+        if (avatarSlot) {
+            this.profileAvatar = new Avatar({
+                src: this.props.profileMainInfo.avatarUrl,
+                class: this.props.type === "private_profile" ? "private-profile-avatar" : "profile-avatar"
+            });
+            this.profileAvatar.mount(avatarSlot as HTMLElement);
+        }
 
+        if (this.props.type === "private_profile") {
+            const avatarContainer = this.element.querySelector('.profile-info__avatar-container');
+            if (avatarContainer) {
+                avatarContainer.addEventListener('click', this.handleAvatarClick);
+            }
+
+            const nameFormSlot = this.element.querySelector('[data-component="full-name-form-slot"]');
+            if (nameFormSlot) {
                 this.settingsFullNameForm = new SettingsFullNameForm({
                     firstName: this.props.profileMainInfo.firstName,
                     lastName: this.props.profileMainInfo.lastName,
                     onChange: this.props.onInput,
                 });
-                this.settingsFullNameForm.mount(this.element!);
-                break;
+                this.settingsFullNameForm.mount(nameFormSlot as HTMLElement);
+            }
         }
     };
 
+    /**
+     * Обработчик клика по аватарке.
+     * @private
+     */
+    private handleAvatarClick = () => {
+        const avatarContainer = this.element?.querySelector('.profile-info__avatar-container');
+        if (avatarContainer) {
+            this.props.onAvatarEditClick?.(avatarContainer as HTMLElement);
+        }
+    };
+
+    /**
+     * @override
+     */
     protected beforeUnmount(): void {
+        const avatarContainer = this.element?.querySelector('.profile-info__avatar-container');
+        if (avatarContainer) {
+            avatarContainer.removeEventListener('click', this.handleAvatarClick);
+        }
+
         this.settingsFullNameForm?.unmount();
-        this.wrapOverlay?.unmount();
         this.profileAvatar?.unmount();
     };
 };
