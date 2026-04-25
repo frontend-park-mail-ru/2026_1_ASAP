01c7b7647ff69ce14b02df1839e733bd4192745a
Aleksandr Vindman <vindmanaleksandr@gmail.com>
2026-04-25 13:05:14 +0300

    Added overlay form

--- src/components/composite/supportOverlay/supportOverlay.ts
+++ src/components/composite/supportOverlay/supportOverlay.ts
@@ -1,6 +1,6 @@
 import { BaseComponent, IBaseComponentProps } from "../../../core/base/baseComponent";
-import { Router } from "../../../core/router";
 import { Button } from "../../ui/button/button";
+import { Input } from "../../ui/input/input";
 import template from "./supportOverlay.hbs";
 
 interface SupportOverlayProps extends IBaseComponentProps {
@@ -9,6 +9,9 @@ interface SupportOverlayProps extends IBaseComponentProps {
 export class SupportOverlay extends BaseComponent<SupportOverlayProps> {
     private state: 'menu' | 'newChat' | 'stats' | null = null;
     private buttonExit: Button | null = null;
+    private emailInput: Input | null = null;
+    private textInput: Input | null = null;
+    private filesInput: Input | null = null;
 
     constructor(props: SupportOverlayProps) {
         super(props);
@@ -19,10 +22,32 @@ export class SupportOverlay extends BaseComponent<SupportOverlayProps> {
     };
 
     protected afterMount(): void {
-        
+        this.buttonExit = new Button({
+            icon: '/assets/icons/deleteIcon.svg'
+        });
+        this.buttonExit.mount(this.element!);
+
+        this.emailInput = new Input({
+            required: true
+        });
+        this.emailInput.mount(this.element!);
+
+        this.textInput = new Input({
+            placeholder: "Например: при отправке голосового сообщения\nинтерфейс зависает на 3-4 секунды...",
+            required: true,
+        });
+        this.textInput.mount(this.element!);
+
+        this.filesInput = new Input({
+            required: true
+        });
+        this.filesInput.mount(this.element!);
     };
 
     protected beforeUnmount(): void {
-        
+        this.buttonExit?.unmount();
+        this.emailInput?.unmount();
+        this.textInput?.unmount();
+        this.filesInput?.unmount();
     };
 };
